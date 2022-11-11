e3b54d08ba1096c212802c1767f50e0bf2bd5a26
jheasley02 <heaslejn@mail.uc.edu>
2022-11-11 00:51:52 -0500

    part 2

---
+++ Scene1.js
@@ -0,0 +1,12 @@
+const { Phaser } = require("./phaser.min");
+
+class Scene1 extends Phaser.Scene{
+  constructor(){
+    super("bootGame");
+  }
+
+  create(){
+    this.add.text(20,20, "Loading game...");
+    this.scene.start("playGame");
+  }
+}
